e602362ba97296b7902bad96c8fa2f62fcdd9104
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-12-09 11:15:17 +0900

    add examples string

--- hello_world/lesson2.js
+++ hello_world/lesson2.js
@@ -7,3 +7,16 @@ console.log(2**3);
 
 // 나머지
 console.log(7%3);
+
+
+// 문자열
+console.log("문자열");
+console.log("I'm Iron man");
+console.log("He said \"I'm Iron man\"");
+
+// backtick (`)
+console.log(`He said "I'm Iron man"`);
+
+// 문자열 병합
+console.log("3"+"Hello!!");
+console.log("3"+"5");
