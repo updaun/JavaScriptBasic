d7df7b0fd42a8a490cec04a549711703ca01d927
updaun <updauney@daum.net>
2021-12-10 11:24:03 +0900

    add examples boolean

--- hello_world/lesson2.js
+++ hello_world/lesson2.js
@@ -20,3 +20,15 @@ console.log(`He said "I'm Iron man"`);
 // 문자열 병합
 console.log("3"+"Hello!!");
 console.log("3"+"5");
+
+// 불린 (Boolean)
+console.log(2>1);
+console.log(2<1);
+console.log(3>=2);
+console.log(3<=3);
+console.log(3===3);
+console.log(3!==3);
+
+console.log('javascript'==='javascript');
+
+console.log(true&&true);
