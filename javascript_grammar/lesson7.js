0a8b595d96a5a0721ea7e43bcb66770fb52970a9
Up & Daun <updauney@daum.net>
2021-12-31 17:23:39 +0900

    211231 TIL

--- javascript_grammar/lesson7.js
+++ javascript_grammar/lesson7.js
@@ -98,3 +98,208 @@ console.log(Math.random());
 
 // 더 많은 Math 메소드
 // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+let sum = 0.1 + 0.2;
+
+// 숫자 계산 오류 발생
+console.log(sum);                     // 0.30000000000000004
+
+// 숫자 계산 오류 해결 방법
+console.log(Number(sum.toFixed(1)));  // 0.3
+console.log(Math.round(sum*10)/10);   // 0.3
+
+// String(문자열도 객체이다!)
+let myString = "  Hello Javascript ";
+
+// 문자열 길이
+console.log(myString.length);
+
+// 요소 접근
+console.log(myString[3]);
+console.log(myString.charAt(3));
+
+// 요소 탐색
+console.log(myString.indexOf('a'));     // 앞에서
+console.log(myString.indexOf('k'));     // 없는 문자열은 -1
+console.log(myString.lastIndexOf('a')); // 뒤에서
+
+// 대소문자 변환
+console.log(myString.toUpperCase()); // 대문자
+console.log(myString.toLowerCase()); // 소문자
+
+// 양 끝 공백 제거
+console.log(myString.trim()); // trim 메소드
+
+// 부분 문자열 접근 slice(start, end)
+console.log(myString.slice(3, 5));
+console.log(myString.slice(6));
+console.log(myString.slice());
+
+
+for (let str of myString) {
+    console.log(str);
+  }
+
+let myString2 = 'hello';
+let myArray2 = ['h','e','l','l','o'];
+
+console.log(typeof myString2);
+console.log(typeof myArray2);
+
+console.log(myString2 === myArray2);
+console.log(myString2 == myArray2);
+
+// 배열은 mutable
+myArray2[0] = 'H';
+console.log(myArray2);
+
+// 문자열은 immutable
+myString2[0] = 'H';
+console.log(myString2);
+
+
+// 기본형과 참조형
+// 기본형(Primitive Type) : Number, String, Boolean, Null, Undefinded
+// 기본형은 변수에 값을 할당한다.
+// 참조형(Reference Type) : Object
+// 참조형은 변수에 주소값을 할당한다.
+
+// 기본형의 변수값 수정
+let x = 3;
+let y = x;
+console.log(x);  // 3
+console.log(y);  // 3
+y = 5;
+console.log(x);  // 3
+console.log(y);  // 5
+
+let ob_x = {name:'updaun'};
+let ob_y = ob_x;
+console.log(ob_x);  // { name: 'updaun' }
+console.log(ob_y);  // { name: 'updaun' }
+ob_y.birth = 2021;
+console.log(ob_x);  // { name: 'updaun', birth: 2021 }
+console.log(ob_y);  // { name: 'updaun', birth: 2021 }
+
+// 배열도 참조형이다.
+let arr_1 = [1,2,3];
+let arr_2 = arr_1;
+console.log(arr_1);
+console.log(arr_2);
+arr_2.push(4);
+console.log(arr_1);
+console.log(arr_2);
+
+
+// 참조형 복사하기 (Reference Type Copy)
+// 복사해서 수정하기!
+// 배열
+let number1 = [1,2,3];
+let number2 = number1.slice();
+
+number2.push(4);
+
+console.log(number1);
+console.log(number2);
+
+// 객체
+let study1 = {
+    title : 'aws saa',
+    subject : 'cloud computing'
+}
+
+let study2 = Object.assign({}, study1);
+
+study2.title = 'Amazon Web Services';
+
+console.log(study1);
+console.log(study2);
+
+
+// 메소드를 사용하지 않고 copy 구현하기
+let study3 = {
+    title : 'coding test',
+    subject : 'algorithm'
+};
+
+let study4 = {};
+
+for (key in study3){
+    study4[key] = study3[key];
+}
+
+study4.title = '자료 구조';
+
+console.log(study3);
+console.log(study4);
+
+// 함수로 구현해보기
+function cloneObject(object) {
+    let temp = {};
+    for (let key in object){
+        temp[key] = object[key];
+    }
+    return temp;
+}
+
+let study5 = {
+    title : 'c++ programming',
+    subject : 'embedded'
+};
+
+let study6 = cloneObject(study5);
+
+study6.title = 'ROS programming';
+
+console.log(study5);
+console.log(study6);
+
+
+// 참조형 변수 안에 또 참조형 변수가 있는 2중 구조의 객체 복사(에러 참고용)
+let study7 = {
+    title : 'computer vision',
+    subject : 'deep learning',
+    prerequisite : []
+};
+
+let study8 = cloneObject(study7);
+
+study8.title = 'object detection';
+// study8 배열에 추가 되었는데, study7에도 추가되어버린다. 참조형 변수 이므로
+study8.prerequisite.push('opencv');
+
+console.log(study7);
+console.log(study8);
+
+function deepcloneObject(object) {
+    if (object === null || typeof object !== 'object'){
+        return object;
+    }
+
+    let temp = {};
+    if (Array.isArray(object)){
+        temp = [];
+    } else {
+        temp = {};
+    }
+
+    for (let key of Object.keys(object)){
+        temp[key] = deepcloneObject(object[key]);
+    }
+    return temp;
+}
+
+let study9 = {
+    title : 'computer vision',
+    subject : 'deep learning',
+    prerequisite : []
+};
+
+let study10 = deepcloneObject(study9);
+
+study10.title = 'object detection';
+// study10에만 추가
+study10.prerequisite.push('opencv');
+
+console.log(study9);
+console.log(study10);
